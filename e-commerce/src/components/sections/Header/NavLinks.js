5abc70588ddbe452fe8cef40127f4b66a6f44550
shauek97 <shauek97@gmail.com>
2023-07-22 20:01:40 +0200

    Added hiding and showing input

--- e-commerce/src/components/sections/Header/NavLinks.js
+++ e-commerce/src/components/sections/Header/NavLinks.js
@@ -1,16 +1,23 @@
 import styles from "./Header.module.scss";
-
+import { AiOutlineSearch } from "react-icons/ai";
 import { useState } from "react";
 
 
-export default function NavLinks(props) {
+
+export default function NavLinks() {
+
+  const [inputVisible, setInputVisible] = useState(false)
+
   return (
     <>
     <h2>O nas</h2>
     <h2>O nas</h2>
     <h2>O nas</h2>
     <h2>O nas</h2>
+  
+    {inputVisible ? <input placeholder="search"></input> : null}
 
+    <div className={styles.searchIcon} onClick={() => setInputVisible(!inputVisible)}><AiOutlineSearch/></div>
    </>
   );
 }
